c1cb33f63678182123a114bce984409882e18a60
Egor <egor29200125@gmail.com>
2021-02-22 19:15:11 +0300

    fix: code refactoring

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,6 +1,8 @@
 import { call, put, select, takeEvery } from 'redux-saga/effects'
-import { SET_BASE_PRIMARY_TYPE, UPDATE_DATA_LIST_VALUES, SET_BASE_PRIMARY_VALUE, SET_BASE_SECONDARY_VALUE, UPDATE_BASE_SECONDARY_VALUE, UPDATE_BASE_PRIMARY_VALUE, SWAP_BASE_VALUES, SET_BASE_SECONDARY_TYPE } from './../constants/actions'
+import { SET_BASE_PRIMARY_TYPE, SET_BASE_PRIMARY_VALUE, SET_BASE_SECONDARY_VALUE, SWAP_BASE_VALUES, SET_BASE_SECONDARY_TYPE } from './../constants/actions'
 import { CurrenciesAPI } from './../api/api'
+import { updateDataListValues } from '../actions/dataList'
+import { updateBasePrimaryValue, updateBaseSecondaryValue } from '../actions/baseValues'
 
 export default function* () {
   yield takeEvery(SET_BASE_PRIMARY_TYPE, getDataListWorker)
@@ -12,47 +14,46 @@ export default function* () {
 
 function* getDataListWorker(action) {
   const data = yield call(getDataListFetch, action.payload)
-  yield put({ type: UPDATE_DATA_LIST_VALUES, payload: data })
+  yield put(updateDataListValues(data))
   const baseCoefficient = yield getBaseCoefficient()
   const currentValue = yield select(state => state.baseValues.primary.value)
-  yield put({ type: UPDATE_BASE_SECONDARY_VALUE, payload: String(+currentValue / baseCoefficient) })
+  yield put(updateBaseSecondaryValue(String(+currentValue * baseCoefficient)))
 }
-
 
 function* updateSecondaryValueWorker() {
   const baseCoefficient = yield getBaseCoefficient()
   const currentValue = yield select(state => state.baseValues.primary.value)
-  yield put({ type: UPDATE_BASE_SECONDARY_VALUE, payload: String(+currentValue * baseCoefficient) })
+  yield put(updateBasePrimaryValue(String(+currentValue / baseCoefficient)))
 }
 
 function* swapBaseValuesWorker() {
   const primaryType = yield select(state => state.baseValues.primary.type)
   const primaryValue = yield select(state => state.baseValues.primary.value)
   const data = yield call(getDataListFetch, primaryType)
-  yield put({ type: UPDATE_DATA_LIST_VALUES, payload: data })
+  yield put(updateDataListValues(data))
   const baseCoefficient = yield getBaseCoefficient()
-  yield put({ type: UPDATE_BASE_SECONDARY_VALUE, payload: String(primaryValue * baseCoefficient) })
-}
-
-async function getDataListFetch(base) {
-  const data = await CurrenciesAPI.getDataListByBase(base).then((res) => res.data.rates)
-  return data
+  yield put(updateBaseSecondaryValue(String(primaryValue * baseCoefficient)))
 }
 
 function* calculateSecondaryWorker(action) {
-  yield put({ type: UPDATE_BASE_PRIMARY_VALUE, payload: action.payload })
+  yield put(updateBasePrimaryValue(action.payload))
   const baseCoefficient = yield getBaseCoefficient()
-  yield put({ type: UPDATE_BASE_SECONDARY_VALUE, payload: String(+action.payload * baseCoefficient) })
+  yield put(updateBaseSecondaryValue(String(+action.payload * baseCoefficient)))
 }
 
 function* calculatePrimaryWorker(action) {
-  yield put({ type: UPDATE_BASE_SECONDARY_VALUE, payload: action.payload })
+  yield put(updateBaseSecondaryValue(action.payload))
   const baseCoefficient = yield getBaseCoefficient()
-  yield put({ type: UPDATE_BASE_PRIMARY_VALUE, payload: String(+action.payload / baseCoefficient) })
+  yield put(updateBasePrimaryValue(String(+action.payload / baseCoefficient)))
 }
 
 function* getBaseCoefficient() {
   const baseType = yield select(state => state.baseValues.secondary.type)
   const baseCoefficient = yield select(state => state.dataList[baseType] || 1)
   return baseCoefficient
+}
+
+async function getDataListFetch(base) {
+  const data = await CurrenciesAPI.getDataListByBase(base).then((res) => res.data.rates)
+  return data
 }
