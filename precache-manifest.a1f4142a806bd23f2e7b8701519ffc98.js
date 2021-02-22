85e8fd0fd372e022c6cedac6ebe2a8521dc15307
Egor <egor29200125@gmail.com>
2021-02-22 13:58:40 +0300

    Updates

---
+++ precache-manifest.a1f4142a806bd23f2e7b8701519ffc98.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c58529330bb5ac5e53d16fd8f72aa8b",
+    "url": "/currency-converter/index.html"
+  },
+  {
+    "revision": "bf45900f6351255b6d79",
+    "url": "/currency-converter/static/js/2.b62308f3.chunk.js"
+  },
+  {
+    "revision": "016a7b8383d5e64915c9",
+    "url": "/currency-converter/static/js/3.eb77e7ca.chunk.js"
+  },
+  {
+    "revision": "269fe86ce39b044b87eb",
+    "url": "/currency-converter/static/js/4.55b40a29.chunk.js"
+  },
+  {
+    "revision": "16fa4d5caeff6efa9eaa",
+    "url": "/currency-converter/static/js/5.b9f4a484.chunk.js"
+  },
+  {
+    "revision": "fbfb1c5d7470f27bf49d",
+    "url": "/currency-converter/static/js/main.61ee604a.chunk.js"
+  },
+  {
+    "revision": "5632d76f72a88764cca4",
+    "url": "/currency-converter/static/js/runtime~main.c0486d0e.js"
+  },
+  {
+    "revision": "46001f4c1cd0c7018309330df7044c72",
+    "url": "/currency-converter/static/media/author-icon.46001f4c.svg"
+  },
+  {
+    "revision": "5917ef301a00200e69844a4efe02e9a2",
+    "url": "/currency-converter/static/media/bg.5917ef30.jpg"
+  },
+  {
+    "revision": "2b4d5ffa416dc0e8940b953c861c4b38",
+    "url": "/currency-converter/static/media/download-icon.2b4d5ffa.svg"
+  },
+  {
+    "revision": "12e46e00e82ecaee8ab3072d43106134",
+    "url": "/currency-converter/static/media/swap-icon.12e46e00.svg"
+  },
+  {
+    "revision": "33d62ed99b37e5c443d20d78a86a545b",
+    "url": "/currency-converter/static/media/version-icon.33d62ed9.svg"
+  }
+]);
